7f192e137f0a00128e6c4e3ae82f67bcf14c1079
rahmanabdul2 <rahmanshahram@gmail.com>
2023-03-02 13:23:39 -0500

    includes the IP, port, name, name and the move keys

---
+++ constants.js
@@ -0,0 +1,23 @@
+// Examples of configuration constants in this project are the IP and PORT information.
+
+
+const IP = '172.19.250.236';
+const PORT = 50541;
+const name = 'AFG';
+const moveUpKey = 'w';
+const moveDownKey = 's';
+const moveLeftKey = 'a';
+const moveRightKey = 'd';
+
+
+
+
+module.exports = {
+  IP,
+  PORT,
+  name,
+  moveDownKey,
+  moveLeftKey,
+  moveUpKey,
+  moveRightKey,
+};
