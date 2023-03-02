4f3c48045a518e67209ba1cab06ed71fe8110438
rahmanabdul2 <rahmanshahram@gmail.com>
2023-03-02 13:22:15 -0500

    client successfully connects to the server

--- client.js
+++ client.js
@@ -1,36 +1,37 @@
-// This will serve as our main file through which we will launch the game client
+// This will serve as your main file through which you will launch the game client.
+// This is the client's side
+
 const net = require("net");
+const { IP, PORT, name} = require('./constants')
 
 // establishes a connection with the game server
-const connectToServer = () => {
+const connect = function() {
   const conn = net.createConnection({
-    host: '172.31.42.213',
-    port: 50541
+    host: IP,
+    port: PORT,
+    name: name
+  });
+
+  // once the connection is successful, client recieves a message and then sends their name to server.
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: ' + name);
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  // register a handler for the connect event
-  conn.on("connect", () => {
-    console.log("Successfully Connected To Server");
-    // set a username and send to the server to start the game
-    conn.write("Name: AFG");
-  });
-
-  // handling data received from server
+  //handle incoming data from the server and console.log it for the player.
   conn.on("data", (data) => {
-    console.log(data);
+    console.log('Server says: ', data);
   });
 
+  ////The conn object that Node returned to you represents the connection that you have with the server.
   return conn;
 };
 
-console.log('Connecting...');
-connectToServer();
 
 
-module.exports = {
-  connectToServer
-};
 
+// should only connect function
+module.exports = { connect };
